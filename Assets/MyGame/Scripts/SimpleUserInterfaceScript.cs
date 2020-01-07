7d93a71ce1bcc8d9358c03524aad8681addb6f95
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-01-07 14:47:45 +0100

    Modify Script. Attach Script. Declare all variables.

---
+++ Assets/MyGame/Scripts/SimpleUserInterfaceScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleUserInterfaceScript : MonoBehaviour
+{
+
+    public Text Result;
+    public Text Number;
+    public InputField InputField;
+    public Button Add_Btn;
+    public Button Transfer_Btn;
+
+    public int Counter =0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PrintName()
+    {
+        Result.text = InputField.text;
+    }
+
+    public void AddNumber()
+    {
+        Counter++;
+        Number.text = Counter.ToString();
+    }
+
+
+}
